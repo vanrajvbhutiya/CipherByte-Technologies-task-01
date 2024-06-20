a8a63283524a67c8561a49a135b8c78fa5c6061d
vanraj bhutiya <vanrajbhutiya09@gmail.com>
2024-06-20 16:32:54 +0530

    added

---
+++ Guess The Number/GuessNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int) (Math.random() * 100);
+        int userNumber = 0;
+
+        do {
+            System.out.print("Guess my number(1-100) : ");
+            userNumber = sc.nextInt();
+
+            if (userNumber == myNumber) {
+                System.out.println("WOOHOO .. CORRECT NUMBER!!!");
+                break;
+            } else if (userNumber > myNumber) {
+                System.out.println("Your number is too large");
+            } else {
+                System.out.println("Your number is too small");
+            }
+
+        } while (userNumber >= 0);
+
+        System.out.print("My Number was :");
+        System.out.println(myNumber);
+    }
+};
